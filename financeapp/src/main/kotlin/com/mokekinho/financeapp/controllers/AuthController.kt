4bb70b420f5db0258f0905064507cc99e52daaac
Moises Protazio <moisesprotazio@gmail.com>
2026-01-28 12:37:03 -0300

    feat: initial of the basic Idea of login

--- financeapp/src/main/kotlin/com/mokekinho/financeapp/controllers/AuthController.kt
+++ financeapp/src/main/kotlin/com/mokekinho/financeapp/controllers/AuthController.kt
@@ -1,11 +1,44 @@
 package com.mokekinho.financeapp.controllers
 
+import com.mokekinho.financeapp.dto.UserDto
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
+@RestController
+@RequestMapping("/auth")
 class AuthController {
-    fun getAuth(){
+
+    private val users = listOf<UserDto>(
+        UserDto(
+            name = "user1",
+            password = "1234"
+        ),
+        UserDto(
+            name = "user2",
+            password = "1234"
+        ),
+        UserDto(
+            name = "user3",
+            password = "1234"
+        ),
+        UserDto(
+            name = "user4",
+            password = "1234"
+        ),
+    )
+
+
+    @PostMapping
+    fun logIn(
+        @RequestBody user: UserDto
+    ){
+
+
+
 
     }
 }
---
+++ financeapp/src/main/kotlin/com/mokekinho/financeapp/dto/UserDto.kt
@@ -0,0 +1,6 @@
+package com.mokekinho.financeapp.dto
+
+data class UserDto(
+    val name: String,
+    val password: String,
+)
