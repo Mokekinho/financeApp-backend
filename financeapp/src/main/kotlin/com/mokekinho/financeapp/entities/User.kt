ed230e256947e724c1d08405bc881a8b386b29f1
Moises Protazio <moisesprotazio@gmail.com>
2026-02-05 16:26:44 -0300

    feat: add anotation for JPA works on UserEntity

--- financeapp/src/main/kotlin/com/mokekinho/financeapp/entities/User.kt
+++ financeapp/src/main/kotlin/com/mokekinho/financeapp/entities/User.kt
@@ -1,12 +1,39 @@
 package com.mokekinho.financeapp.entities
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 
-//@Entity//Todo criar um banco de dados pra guardar isso
-data class User(
-    val id: Long,
-    val name: String,
-    val password: String,
-    val role: Role = Role.USER
+
+@Entity
+@Table(
+    name = "users"
+)
+class User(
+    @Id
+    @GeneratedValue(
+        strategy = GenerationType.IDENTITY
+    )
+    val id: Long = 0 ,
+
+    @Column(
+        nullable = false,
+        unique = true
+    ) val name: String = "",
+
+    @Column(
+        nullable = false
+    ) val password: String = "",
+
+    @Enumerated(EnumType.STRING)
+    @Column(
+        nullable = false
+    ) val role: Role = Role.USER
 )
 
 enum class Role(){
